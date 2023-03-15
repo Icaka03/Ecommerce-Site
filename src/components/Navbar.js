a213af8c42527f6dd1d622344802c4148c14c16f
Hristo <generatorfox@gmail.com>
2023-03-15 16:15:44 +0200

    add status when logged

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,16 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 export default function Navbar() {
   const [accountLink, setAccountLink] = useState("");
   const auth = getAuth();
+  let [status, setStatus] = useState("");
   onAuthStateChanged(auth, (user) => {
     setAccountLink(user ? "/account" : "/register");
+  });
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setStatus("logged");
+    } else {
+      setStatus("not logged");
+    }
   });
   return (
     <div className={styles.header}>
@@ -37,6 +45,7 @@ export default function Navbar() {
         </li>
         <li>
           <Link to={accountLink}>Account</Link>
+          <p className={styles.status}>{status}</p>
         </li>
       </ul>
     </div>
